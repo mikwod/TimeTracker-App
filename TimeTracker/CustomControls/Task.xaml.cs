26f3d7c3e9eafc11b83b607310d945ebce70f104
mikwod <mikwod@MSI7700K>
2019-02-23 14:44:09 -0600

    Convert elapsed time to half hour increments

--- TimeTracker/CustomControls/Task.xaml.cs
+++ TimeTracker/CustomControls/Task.xaml.cs
@@ -92,6 +92,7 @@ namespace TimeTracker
             {
                 this._dispatcherTimer.Stop();
                 this._stopWatch.Stop();
+                ConvertElapsedTimeToHours();
                 this._stopWatch.Reset();
                 this.StartStopButton.Content = this.TimeText.Text != this._startTime ? "Time Recorded" : "Start";
                 this.StartStopButton.Background = this.TimeText.Text != this._startTime ? _startButtonColorWithTime : _startButtonColor;
@@ -102,10 +103,18 @@ namespace TimeTracker
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
             // Updating the Label which displays the current second
-            this.TimeText.Text = _stopWatch.Elapsed.ToString(@"hh\:mm\:ss");
+            this.TimeText.Text = this._stopWatch.Elapsed.ToString(@"hh\:mm\:ss");
 
             // Forcing the CommandManager to raise the RequerySuggested event
             CommandManager.InvalidateRequerySuggested();
+        }
+
+        private void ConvertElapsedTimeToHours()
+        {
+            if(this.TimeText.Text != this._startTime)
+            {
+                this.TimeText.Text = (Math.Ceiling(this._stopWatch.Elapsed.TotalHours / 0.5) * 0.5).ToString() + " hours";
+            }
         }
 
         #endregion
